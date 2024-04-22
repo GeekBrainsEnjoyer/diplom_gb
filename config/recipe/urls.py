db5eed7de5d46bb24b9d193bc33dc6e6f15033aa
sh <rahmatylin.sasha@yandex.ru>
2024-04-22 06:19:52 +0300

    +forms

--- config/recipe/urls.py
+++ config/recipe/urls.py
@@ -6,4 +6,7 @@ app_name = 'recipe'
 
 urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/edit_recipe/', views.edit_recipe, name='edit_recipe'),
+    path(route='<int:pk>/delete/', view=views.delete_recipe, name='delete_recipe'),
+    path('new_recipe/', view=views.new_recipe, name='new_recipe'),
 ]
--- config/recipe/views.py
+++ config/recipe/views.py
@@ -1,9 +1,56 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
+
+
+from .forms import RecipeForm
 
 
 def detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipe/detail.html', {
         'recipe': recipe})
+
+
+@login_required
+def new_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            recipe = form.save(False)
+            recipe.created_by = request.user
+            recipe.save()
+
+            return redirect('recipe:detail', pk=recipe.id)
+
+    else:
+        form = RecipeForm()
+
+    return render(request, 'recipe/recipe_form.html', {'form': form, 'title': 'Новый рецепт'})
+
+
+@login_required
+def edit_recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+
+        if form.is_valid():
+            recipe.save()
+
+            return redirect('recipe:detail', pk=recipe.id)
+
+    else:
+        form = RecipeForm(instance=recipe)
+
+    return render(request, 'recipe/recipe_form.html', {'form': form, 'title': 'Изменить рецепт'})
+
+
+@login_required
+def delete_recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk, created_by=request.user)
+    recipe.delete()
+
+    return redirect('core:index')
